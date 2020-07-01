a59e9209d20dcc07dad22c8f4f9de900400e0e7c
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-07-01 13:21:47 +0700

    update

--- src/config/suite.cucumber.conf.js
+++ src/config/suite.cucumber.conf.js
@@ -1,63 +1,68 @@
 const defaultTimeoutInterval = process.env.DEBUG ? (60 * 60 * 500) : 90000;
 
 exports.config = {
-    specs: [
-        './src/features/loginFailed.feature',
-    ],
-    exclude: [],
-    maxInstances: 10,
+  specs: [
+    './src/features/*.feature',
+  ],
+  exclude: [],
+  maxInstances: 10,
 
-    capabilities: [
-          {
-              browserName: 'chrome',
-              maxInstances: '5',
-          },
-    ],
-    sync: true,
-    logLevel: 'info',
-    coloredLogs: true,
-    screenshotPath: './test/reports/errorShots/',   // Saves a screenshot to a given path if a command fails.
-    baseUrl: 'https://web.qa.leapxpert.app/',
-    waitforTimeout: 90000,            // Default timeout for all waitFor* commands.
-    connectionRetryTimeout: 90000,    // Default timeout in milliseconds for request  if Selenium Grid doesn't send response
-    connectionRetryCount: 3,          // Default request retries count
-    reporters: [
-      'spec',
-    ],
-
-    services: ['selenium-standalone'],
-    framework: 'cucumber',
-    cucumberOpts: {
-        requireModule: ['@babel/register'],
-        require: ['./src/stepDefinitions/given.js', './src/stepDefinitions/when.js', './src/stepDefinitions/then.js'],   // <string[]> (file/dir) require files before executing features
-        backtrace: true,    // <boolean> show full backtrace for errors
-        //compiler: ['js:babel-core/register'], // <string[]> filetype:compiler used for processing required features
-        compiler: [], // <string[]> filetype:compiler used for processing required features
-        failAmbiguousDefinitions: true,       // <boolean< Treat ambiguous definitions as errors
-        dryRun: false,      // <boolean> invoke formatters without executing steps
-        failFast: false,    // <boolean> abort the run on first failure
-        ignoreUndefinedDefinitions: false,    // <boolean> Enable this config to treat undefined definitions as warnings
-        name: [],           // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-        snippets: true,     // <boolean> hide step definition snippets for pending steps
-        format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
-        colors: true,       // <boolean> disable colors in formatter output
-        snippets: false,    // <boolean> hide step definition snippets for pending steps
-        source: false,      // <boolean> hide source uris
-        profile: [],        // <string[]> (name) specify the profile to use
-        strict: true,       // <boolean> fail if there are any undefined or pending steps
-        tagExpression: 'not @Pending',      // <string> (expression) only execute the features or scenarios with tags matching the expression, see https://docs.cucumber.io/tag-expressions/
-        timeout: defaultTimeoutInterval,    // <number> timeout for step definitions
-        tagsInTitle: false,                 // <boolean> add cucumber tags to feature or scenario name
-        snippetSyntax: undefined,           // <string> specify a custom snippet syntax
+  capabilities: [
+    {
+      browserName: 'chrome',
+      maxInstances: 1,
     },
+  ],
+  sync: true,
+  logLevel: 'info',
+  coloredLogs: true,
+  screenshotPath: './test/reports/errorShots/',   // Saves a screenshot to a given path if a command fails.
+  baseUrl: 'https://web.qa.leapxpert.app/',
+  waitforTimeout: 90000,            // Default timeout for all waitFor* commands.
+  connectionRetryTimeout: 90000,    // Default timeout in milliseconds for request  if Selenium Grid doesn't send response
+  connectionRetryCount: 3,          // Default request retries count
+  reporters: [
+    'spec',
+    ['allure', {
+      outputDir: './reports/allure-results',
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: true,
+    }]
+  ],
 
-    before: function() {
-      /**
-       * Setup the Chai assertion framework
-       */
-      const chai    = require('chai');
-      global.expect = chai.expect;
-      global.assert = chai.assert;
-      global.should = chai.should();
-    },
+  services: ['selenium-standalone'],
+  framework: 'cucumber',
+  cucumberOpts: {
+    requireModule: ['@babel/register'],
+    require: ['./src/stepDefinitions/given.js', './src/stepDefinitions/when.js', './src/stepDefinitions/then.js'],   // <string[]> (file/dir) require files before executing features
+    backtrace: true,    // <boolean> show full backtrace for errors
+    //compiler: ['js:babel-core/register'], // <string[]> filetype:compiler used for processing required features
+    compiler: [], // <string[]> filetype:compiler used for processing required features
+    failAmbiguousDefinitions: true,       // <boolean< Treat ambiguous definitions as errors
+    dryRun: false,      // <boolean> invoke formatters without executing steps
+    failFast: false,    // <boolean> abort the run on first failure
+    ignoreUndefinedDefinitions: false,    // <boolean> Enable this config to treat undefined definitions as warnings
+    name: [],           // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    snippets: true,     // <boolean> hide step definition snippets for pending steps
+    format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    colors: true,       // <boolean> disable colors in formatter output
+    snippets: false,    // <boolean> hide step definition snippets for pending steps
+    source: false,      // <boolean> hide source uris
+    profile: [],        // <string[]> (name) specify the profile to use
+    strict: true,       // <boolean> fail if there are any undefined or pending steps
+    tagExpression: 'not @Pending',      // <string> (expression) only execute the features or scenarios with tags matching the expression, see https://docs.cucumber.io/tag-expressions/
+    timeout: defaultTimeoutInterval,    // <number> timeout for step definitions
+    tagsInTitle: false,                 // <boolean> add cucumber tags to feature or scenario name
+    snippetSyntax: undefined,           // <string> specify a custom snippet syntax
+  },
+
+  before: function () {
+    /**
+     * Setup the Chai assertion framework
+     */
+    const chai = require('chai');
+    global.expect = chai.expect;
+    global.assert = chai.assert;
+    global.should = chai.should();
+  },
 };
